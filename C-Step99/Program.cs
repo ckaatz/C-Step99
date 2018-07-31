4c915e2984c3fd965076a7feab1b740049c83809
ckaatz <39869540+ckaatz@users.noreply.github.com>
2018-07-31 18:40:01 -0500

    Finish additional steps

--- C-Step99/Program.cs
+++ C-Step99/Program.cs
@@ -12,12 +12,49 @@ namespace C_Step99
         {
             string[] Days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
             Console.WriteLine("Pick a number between 0 and 6:");
-            Console.WriteLine(Days[Convert.ToInt32(Console.ReadLine())]);
-            Console.ReadLine();
+            int Array1 = Convert.ToInt32(Console.ReadLine());
+            if (Array1 >= 0 && Array1 <= 6)
+            {
+                Console.WriteLine(Days[Array1]);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("You picked a number outside of the index.");
+                Console.ReadLine();
+            }
+
 
             int[] Number = { 56, 91, 1, 9, 82, 5 };
             Console.WriteLine("Pick a number between 0 and 5:");
-            Console.WriteLine(Number[Convert.ToInt32(Console.ReadLine())]);
+            int Array2 = Convert.ToInt32(Console.ReadLine());
+            if (Array2 >= 0 && Array2 <= 5)
+            {
+                Console.WriteLine(Number[Array2]);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("You picked a number outside of the index.");
+                Console.ReadLine();
+            }
+
+            List<string> Months = new List<string>();
+            Months.Add("January");
+            Months.Add("February");
+            Months.Add("March");
+            Months.Add("April");
+            Months.Add("May");
+            Months.Add("June");
+            Months.Add("July");
+            Months.Add("August");
+            Months.Add("September");
+            Months.Add("October");
+            Months.Add("November");
+            Months.Add("December");
+            Console.WriteLine("Pick a number between 0 and 11:");
+            int List1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Months[List1]);
             Console.ReadLine();
         }
     }
